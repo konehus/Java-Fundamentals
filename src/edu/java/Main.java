f53ee27c0e1b3471bd63bd3b198a0f57ea20e1f8
hendrix <konehus@hotmail.com>
2021-03-09 22:39:38 -0600

    Project Local repository moved to Documents folder

---
+++ src/edu/java/Main.java
@@ -0,0 +1,13 @@
+package edu.java;
+
+import java.util.Arrays;
+
+public class Main {
+
+    //main method begins execution of java application
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming");
+
+        System.out.println(Arrays.toString(args));
+    }
+}
---
+++ src/edu/java/datatypes/DataTypes.java
@@ -0,0 +1,5 @@
+package edu.java.datatypes;
+
+public class DataTypes {
+
+}
